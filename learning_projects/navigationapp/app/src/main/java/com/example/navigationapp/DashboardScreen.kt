4ed56a7e97dbcefc13373abf8db15107ca1a234f
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2025-01-19 14:06:15 +0530

    Added screens

---
+++ learning_projects/navigationapp/app/src/main/java/com/example/navigationapp/DashboardScreen.kt
@@ -0,0 +1,27 @@
+package com.example.navigationapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DashboardScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Dashboard")
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun DashboardScreenPreview() {
+    DashboardScreen()
+}
---
+++ learning_projects/navigationapp/app/src/main/java/com/example/navigationapp/SignInScreen.kt
@@ -0,0 +1,31 @@
+package com.example.navigationapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SignInScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Sign In")
+        Button(onClick = {}) {
+            Text(text = "Sign In")
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SignInScreenPreview() {
+    SignInScreen()
+}
