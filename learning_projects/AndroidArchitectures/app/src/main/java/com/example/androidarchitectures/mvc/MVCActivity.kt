3853c9601aaaa9d870367c7051e160185d5002dc
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2026-03-21 11:17:40 +0530

    Added the List Layout

--- learning_projects/AndroidArchitectures/app/src/main/java/com/example/androidarchitectures/mvc/MVCActivity.kt
+++ learning_projects/AndroidArchitectures/app/src/main/java/com/example/androidarchitectures/mvc/MVCActivity.kt
@@ -3,15 +3,44 @@ package com.example.androidarchitectures.mvc
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidarchitectures.R
 
 class MVCActivity : AppCompatActivity() {
+
+    private val listValues = ArrayList<String>()
+    private lateinit var list: ListView
+    private lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_mvc)
+
+        list = findViewById(R.id.list)
+        adapter = ArrayAdapter(this, R.layout.row_layout, R.id.listText, listValues)
+
+        list.setAdapter(adapter)
+        list.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, "You clicked " + listValues.get(position), Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        val test = ArrayList<String>()
+        test.add("India")
+        test.add("UK")
+        test.add("America")
+        setValues(test)
+    }
+
+    fun setValues(values: List<String>) {
+        listValues.clear()
+        listValues.addAll(values)
+        adapter.notifyDataSetChanged()
     }
 
     companion object {
