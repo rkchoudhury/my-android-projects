70fde58db33433b3e6eb67a6b23d6a4e94ed550e
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2026-04-07 20:41:06 +0530

    Handle keyboard button

--- learning_projects/CleanArchitecture/app/src/main/java/com/example/cleanarchitecture/presentation/screens/note/NoteScreen.kt
+++ learning_projects/CleanArchitecture/app/src/main/java/com/example/cleanarchitecture/presentation/screens/note/NoteScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Text
@@ -22,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -106,14 +108,20 @@ fun Body(
             onValueChange = onTitleChange,
             label = { Text("Title") },
             singleLine = true,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            )
         )
         Spacer(modifier = Modifier.height(20.dp))
         TextField(
             value = description,
             onValueChange = onDescriptionChange,
             label = { Text("Description") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done
+            )
         )
     }
 }
