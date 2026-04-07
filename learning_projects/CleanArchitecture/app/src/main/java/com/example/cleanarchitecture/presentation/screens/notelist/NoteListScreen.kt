cc09c0683e3ca53d121bf747055a33bb61ad41d4
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2026-04-07 20:36:09 +0530

    Updated Note List

--- learning_projects/CleanArchitecture/app/src/main/java/com/example/cleanarchitecture/presentation/screens/notelist/NoteListScreen.kt
+++ learning_projects/CleanArchitecture/app/src/main/java/com/example/cleanarchitecture/presentation/screens/notelist/NoteListScreen.kt
@@ -9,12 +9,17 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.LifecycleResumeEffect
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.cleanarchitecture.framework.viewmodels.NoteListViewModel
 import com.example.cleanarchitecture.presentation.common.Card
 import com.example.cleanarchitecture.presentation.common.FloatingButton
 import com.example.cleanarchitecture.presentation.common.NaviBar
@@ -22,9 +27,17 @@ import com.example.cleanarchitecture.presentation.navigation.Route
 
 @Composable
 fun NoteListScreen(navController: NavController) {
-    val list = listOf(
-        "A", "B", "C", "D", "E", "F", "A", "B", "C", "D", "E", "F", "D", "E", "F"
-    )
+    val viewModel: NoteListViewModel = viewModel()
+    val notes by viewModel.allNotes.observeAsState()
+
+    LifecycleResumeEffect(Unit) {
+        // This function is called every time the lifecycle enters ON_RESUME
+        viewModel.getAllNotes()
+
+        onPauseOrDispose {
+            // Optional: Clean up when leaving ON_RESUME
+        }
+    }
 
     Column {
         NaviBar("Notes") { navController.popBackStack() }
@@ -33,9 +46,15 @@ fun NoteListScreen(navController: NavController) {
                 modifier = Modifier
                     .fillMaxSize()
             ) {
-                items(items = list, itemContent = { item ->
-                    Card(title = item, content = item.repeat(10))
-                })
+                notes?.let { it ->
+                    if (it.isNotEmpty()) {
+                        items(
+                            items = it.sortedByDescending { it.updateTime },
+                            itemContent = { item ->
+                                Card(title = item.title, content = item.content)
+                            })
+                    }
+                }
             }
 
             FloatingButton(
