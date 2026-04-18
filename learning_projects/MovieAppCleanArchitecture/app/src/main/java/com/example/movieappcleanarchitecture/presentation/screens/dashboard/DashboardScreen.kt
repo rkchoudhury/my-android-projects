ac98c7cd1124d4392ecf0dccc0689777c870f69b
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2026-04-18 12:48:57 +0530

    Added MovieError component

--- learning_projects/MovieAppCleanArchitecture/app/src/main/java/com/example/movieappcleanarchitecture/presentation/screens/dashboard/DashboardScreen.kt
+++ learning_projects/MovieAppCleanArchitecture/app/src/main/java/com/example/movieappcleanarchitecture/presentation/screens/dashboard/DashboardScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.movieappcleanarchitecture.presentation.components.LoadingIndicator
+import com.example.movieappcleanarchitecture.presentation.components.MovieError
 import com.example.movieappcleanarchitecture.presentation.components.MovieGrid
 import com.example.movieappcleanarchitecture.presentation.viewmodels.MovieViewModel
 
@@ -17,12 +18,15 @@ fun DashboardScreen() {
     val movieList by movieViewModel.moviesState
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
         when {
             movieList.loading -> {
                 LoadingIndicator()
+            }
+
+            movieList.error != null -> {
+                MovieError("Something went wrong \n" + movieList.error.toString())
             }
 
             else -> {
