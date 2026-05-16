b8212cd1229a82d9e4f2c8503793a23ed07f9ec8
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2026-05-16 13:26:25 +0530

    Added dragger dependencies

--- learning_projects/Dagger2Car/app/build.gradle.kts
+++ learning_projects/Dagger2Car/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -55,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation("com.google.dagger:dagger:2.59.2")
+    ksp("com.google.dagger:dagger-compiler:2.59.2")
 }
